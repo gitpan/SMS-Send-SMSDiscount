d6f419c918f9e002c37f88853818f67721d61d50
Mark Hindess <soft-cpan@temporalanomaly.com>
2010-09-24 15:50:29 +0000

    Import of BEANZ/SMS-Send-SMSDiscount-1.102671 from CPAN.
    gitpan-cpan-distribution: SMS-Send-SMSDiscount
    gitpan-cpan-version:      1.102671
    gitpan-cpan-path:         BEANZ/SMS-Send-SMSDiscount-1.102671.tar.gz
    gitpan-cpan-author:       BEANZ
    gitpan-cpan-maturity:     released

--- lib/SMS/Send/SMSDiscount.pm
+++ lib/SMS/Send/SMSDiscount.pm
@@ -1,11 +1,14 @@
+use strict;
+use warnings;
 package SMS::Send::SMSDiscount;
+BEGIN {
+  $SMS::Send::SMSDiscount::VERSION = '1.102671';
+}
 
 # ABSTRACT: SMS::Send driver to send via smsdiscount.com
 
 
 use 5.006;
-use strict;
-use warnings;
 use SMS::Send::Driver;
 use LWP::UserAgent;
 use HTTP::Request::Common qw(POST);
@@ -74,7 +77,7 @@ SMS::Send::SMSDiscount - SMS::Send driver to send via smsdiscount.com
 
 =head1 VERSION
 
-version 1.102670
+version 1.102671
 
 =head1 SYNOPSIS
 
